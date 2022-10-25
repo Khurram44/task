d75066c9239feb8dd3ce6d9ecd0478570ab65ceb
Khurram44 <ks7844201@gmail.com>
2022-10-25 18:25:35 +0500

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Gallery from '../components/gallery/Gallery'
+import HomeComponent from '../components/Home/Home'
+
+function Home() {
+  return (
+    <div><HomeComponent/></div>
+  )
+}
+
+export default Home
